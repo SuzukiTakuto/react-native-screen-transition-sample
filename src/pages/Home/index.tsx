2754de94df556d1f50c29f2e2c4d207582360f46
SuzukiTakuto <Largo0907@outlook.jp>
2023-10-10 11:18:26 +0900

    first

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,10 @@
-import React, {FC} from 'react';
-import {Text, Button} from 'react-native';
-import type {RootRoutesParamList} from '../../routes/Root';
-import type {NativeStackScreenProps} from '@react-navigation/native-stack';
+import React, { FC } from 'react'
+import { Text, Button } from 'react-native'
+import type { RootRoutesParamList } from '../../routes/Root'
+import type { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-type Props = NativeStackScreenProps<RootRoutesParamList, 'HomePage'>;
-export const HomePage: FC<Props> = ({navigation}) => {
+type Props = NativeStackScreenProps<RootRoutesParamList, 'HomePage'>
+export const HomePage: FC<Props> = ({ navigation }) => {
   return (
     <>
       <Text>HOME</Text>
@@ -12,6 +12,10 @@ export const HomePage: FC<Props> = ({navigation}) => {
         onPress={() => navigation.push('DetailPage')}
         title="Settings page"
       />
+      <Button
+        onPress={() => navigation.push('VoiceChatPage')}
+        title="VoiceChat page"
+      />
     </>
-  );
-};
+  )
+}
--- src/routes/Root/index.tsx
+++ src/routes/Root/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {SettingsPage} from '../../pages/Settings';
-import {HomePage} from '../../pages/Home';
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { SettingsPage } from '../../pages/Settings'
+import { HomePage } from '../../pages/Home'
+import VoiceChatPage from '../../pages/VoiceChat'
 
-const Stack = createNativeStackNavigator<RootRoutesParamList>();
+const Stack = createNativeStackNavigator<RootRoutesParamList>()
 
 export type RootRoutesParamList = {
-  HomePage: undefined;
-  DetailPage: undefined;
-};
+  HomePage: undefined
+  DetailPage: undefined
+  VoiceChatPage: undefined
+}
 
 export const RootRoutes = () => {
   return (
@@ -16,13 +18,18 @@ export const RootRoutes = () => {
       <Stack.Screen
         component={HomePage}
         name="HomePage"
-        options={{title: 'Home'}}
+        options={{ title: 'Home' }}
       />
       <Stack.Screen
         component={SettingsPage}
         name="DetailPage"
-        options={{title: 'Detail'}}
+        options={{ title: 'Detail' }}
+      />
+      <Stack.Screen
+        component={VoiceChatPage}
+        name="VoiceChatPage"
+        options={{ title: 'VoiceChat' }}
       />
     </Stack.Navigator>
-  );
-};
+  )
+}
