47e2d61a20055a5f03f4af255ab4bcb0909b74ea
SuzukiTakuto <Largo0907@outlook.jp>
2023-10-18 22:55:27 +0900

    fix: props

--- src/components/generics/IconScroll/index.tsx
+++ src/components/generics/IconScroll/index.tsx
@@ -1,6 +1,10 @@
 import React from "react"
 import IconScrollPresenter from "./presenter"
 
+export type Props = {
+  test: string
+}
+
 const IconScroll = () => {
   return <IconScrollPresenter />
 }
--- src/pages/EventRoom/presenter.tsx
+++ src/pages/EventRoom/presenter.tsx
@@ -3,7 +3,7 @@ import { View, ScrollView } from "react-native"
 import { colorCode } from "../../styles/general"
 import InteractiveCircularIcon from "../../components/generics/InteractiveCircularIcon"
 import Tabbar from "../../components/generics/Tabbar"
-import IconScroll from "../../components/generics/IconScroll.tsx"
+import IconScroll from "../../components/generics/IconScroll"
 
 type Props = {}
 
