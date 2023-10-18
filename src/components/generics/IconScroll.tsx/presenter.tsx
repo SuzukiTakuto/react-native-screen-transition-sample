f79debcfb14dbd1787e9aa06babf095f4f020928
SuzukiTakuto <Largo0907@outlook.jp>
2023-10-18 16:07:07 +0900

    feat: IconScroll component

---
+++ src/components/generics/IconScroll.tsx/presenter.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { Image, ScrollView, Text, View } from "react-native"
+import { Props as ContainerProps } from "./index"
+import styles from "./style"
+import InteractiveCircularIcon from "../InteractiveCircularIcon"
+
+type Props = {
+  url: any
+} & ContainerProps
+
+const IconScrollPresenter = () => {
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal style={{ backgroundColor: "black" }}>
+        {[0, 1, 2, 3, 4, 5].map((i) => {
+          return (
+            <InteractiveCircularIcon
+              key={i}
+              url={{ image: require("./yamashita.png") }}
+              name={"yamashita"}
+            />
+          )
+        })}
+      </ScrollView>
+    </View>
+  )
+}
+
+export default IconScrollPresenter
